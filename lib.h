389a8a211890b38af5c2355e579c4ade0bc43d09
F1GIAN <91794502+F1GIAN@users.noreply.github.com>
2021-12-01 20:40:29 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,5 +1,5 @@
-#ifndef 7_NUMERI_PRIMI_LIB_H
-#define 7_NUMERI_PRIMI_LIB_H
+#ifndef ES_7_NUMERI_PRIMI_LIB_H
+#define ES_7_NUMERI_PRIMI_LIB_H
 
-#endif 7_NUMERI_PRIMI_LIB_H
+#endif ES_7_NUMERI_PRIMI_LIB_H
 bool numerip (int, int);
